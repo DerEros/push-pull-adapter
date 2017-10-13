0da53b4becc765dc3c96988360a849cc5cea1206
Eros Candelaresi <eros@candelaresi.de>
2017-10-13 23:17:48 +0200

    Trying to add websocket
    (cherry picked from commit 8e5d2ae)

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,4 +1,10 @@
 var stompClient = null;
+var lastTimeStamp = 0;
+var lastReading = 0;
+var newTimeStamp = 0;
+var newReading = 0;
+
+var counter1UUID = "fde8f1d0-c5d0-11e0-856e-f9e4360ced10";
 
 function setConnected(connected) {
 
@@ -10,9 +16,7 @@ function connect() {
     stompClient.connect({}, function(frame) {
         setConnected(true);
         console.log("Connected: " + frame);
-        stompClient.subscribe('/topic/time', function(time) {
-            showTime(JSON.parse(time.body).time);
-        })
+        stompClient.subscribe('/topic/data', onReceiveData)
     })
 }
 
@@ -24,8 +28,34 @@ function disconnect() {
     console.log("Disconnected");
 }
 
-function showTime(time) {
-    $("#time").append("<p>" + time + "</p>");
+function onReceiveData(data) {
+    var tuple = jsonPath(JSON.parse(data.body), "$.data[?(@.uuid == '" + counter1UUID + "')].tuples[0]");
+
+    newTimeStamp = tuple[0][0];
+    newReading = tuple[0][1];
+
+    showConsumption();
+
+}
+
+function showConsumption() {
+    if (lastTimeStamp === 0 || lastReading === 0) {
+        $("#elapsedTime").html("0");
+        $("#consumedPower").html("0");
+    } else {
+        var elapsedSeconds = (newTimeStamp - lastTimeStamp) / 1000;
+        var elapsedHours = elapsedSeconds / 3600;
+        var diffReading = newReading - lastReading;
+        var consumption = diffReading / elapsedHours;
+
+        $("#elapsedTime").html(elapsedSeconds.toFixed(1) + " s");
+        $("#consumedPower").html(consumption.toFixed(1) + " W");
+    }
+
+    lastReading = newReading;
+    lastTimeStamp = newTimeStamp;
+
+    console.log(lastTimeStamp, lastReading, newTimeStamp, newReading);
 }
 
 $(function () {
