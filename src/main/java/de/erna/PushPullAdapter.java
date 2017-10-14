661ba5906940600c6e8b374850558cb5323297d5
Eros Candelaresi <eros@candelaresi.de>
2017-10-14 21:00:01 +0200

    Combined WS and Rest implementation

--- src/main/java/de/erna/PushPullAdapter.java
+++ src/main/java/de/erna/PushPullAdapter.java
@@ -2,8 +2,10 @@ package de.erna;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicReference;
@@ -11,13 +13,19 @@ import java.util.concurrent.atomic.AtomicReference;
 import static org.springframework.boot.Banner.Mode.OFF;
 
 @RestController
-@EnableAutoConfiguration
+@SpringBootApplication
 public class PushPullAdapter {
     private static Logger log = LoggerFactory.getLogger(PushPullAdapter.class);
 
     private AtomicReference<String> data = new AtomicReference<>("");
+    private SimpMessagingTemplate template;
 
-    @GetMapping("/data")
+    @Autowired
+    public PushPullAdapter(SimpMessagingTemplate template) {
+        this.template = template;
+    }
+
+    @GetMapping("/")
     @ResponseBody
     public String getData() {
         return data.get();
@@ -27,6 +35,7 @@ public class PushPullAdapter {
     public void storeData(@RequestBody String newData) {
         log.debug("Got data: {}", data);
         this.data.set(newData);
+        template.convertAndSend("/topic/data", newData);
     }
 
     public static void main(String[] args) {
